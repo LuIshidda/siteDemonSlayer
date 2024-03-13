c9b9b4afe78eabcdf5d008b02d118d61286f1d86
LuIshidda <luizhenrisansou23@gmail.com>
2024-03-12 22:24:26 -0300

    iniciando projeto

---
+++ DemonSlayer/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DemonSlayer.Models;
+using System.Text.Json;
+
+namespace DemonSlayer.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        List<Mangas> mangas =[];
+        using (StreamReader leitor = new("Data\\mangas.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            mangas = JsonSerializer.Deserialize<List<Mangas>>(dados);
+        }
+        return View(mangas);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
